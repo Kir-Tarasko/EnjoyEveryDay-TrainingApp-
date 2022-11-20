1985af3a2e312d8c3cb61a0c5bb72f6c0f906f34
KirTarasko <kir.tarasko@gmail.com>
2022-11-20 18:41:09 +0200

    Done

--- EnjoyEveryDay(TrainingApp)/ViewController.swift
+++ EnjoyEveryDay(TrainingApp)/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var resultButton: UIButton!
+    
+    private var numberOfDays = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func resultButtonTapped() {
+        
+        infoLabel.text = "Ты наслаждаешься жизнью уже \(numberOfDays) дней!"
+        
+    }
+    
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        
+        let range = sender.date..<Date.now
+        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]))
+        
+    }
 }
 
